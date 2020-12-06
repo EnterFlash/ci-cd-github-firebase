d9a5aebe40dba65634f94b97472979eddf776871
Carlos Eli Lopez Tellez <carloselilopezt@gmail.com>
2020-12-05 18:24:26 -0600

    No moon in planets!

--- src/components/Space.vue
+++ src/components/Space.vue
@@ -7,7 +7,6 @@
         <select id="planets" v-model="planetColor" aria-label="Planets">
           <option value="#ff5f40">Mars</option>
           <option value="#cca770">Venus</option>
-          <option value="#ffffff">Moon</option>
           <option value="#7697c2">Neptune</option>
         </select>
       </div>
